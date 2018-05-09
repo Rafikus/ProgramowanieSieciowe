d93c22c3c3da229030471f09373fdfa6049bdcd8
Rafikus <Rafikus95@gmail.com>
2018-05-09 22:46:43 +0200

    Zrobione zadanie Laboratorium 6

---
+++ UnicastUDPClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UnicastUDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            EndPoint groupEP = new IPEndPoint(ipAddress, 2222);
+
+            while (true)
+            {
+                byte[] msg = Encoding.UTF8.GetBytes(Console.ReadLine()?.ToCharArray());
+                Console.WriteLine($"Sending {Encoding.UTF8.GetString(msg)}");
+                socket.SendTo(msg, groupEP);
+                socket.ReceiveFrom(msg, ref groupEP);
+                Console.WriteLine($"Recieved {Encoding.UTF8.GetString(msg)}");
+            }
+        }
+    }
+}
---
+++ UnicastUDPServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicastUDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(2222);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 2222);
+
+            while (true)
+            {
+                byte[] msg = udpClient.Receive(ref groupEP);
+                Console.WriteLine(Encoding.UTF8.GetString(msg));
+                udpClient.Send(msg, msg.Length, groupEP);
+            }
+        }
+    }
+}
